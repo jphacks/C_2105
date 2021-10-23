369051df76d268f75b158cd60d865cdc31eb404b
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-10-23 12:53:43 +0900

    add socket server

--- arduino/server/server.js
+++ arduino/server/server.js
@@ -1,1 +1,13 @@
-const sp = require('./utils/serialport');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+const io = require('socket.io')(http);
+const PORT = process.env.PORT || 3001;
+
+io.on('connection', (socket) => {
+  console.log('connected!');
+});
+
+http.listen(PORT, function(){
+  console.log('server listening. Port:' + PORT);
+});

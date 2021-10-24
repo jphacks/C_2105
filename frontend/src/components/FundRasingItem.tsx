a902f1add000eb0d841969819a11d80a2147871e
toshikingggg <study.king7777@gmail.com>
2021-10-24 12:24:31 +0900

    結果ページ大枠実装

--- frontend/src/components/FundRasingItem.tsx
+++ frontend/src/components/FundRasingItem.tsx
@@ -6,24 +6,22 @@ type Props = { project: Project; changeProject: (project: Project) => void }
 const FundRasingItem: FC<Props> = ({ project, changeProject }) => {
   return (
     <div
-      className="grid grid-cols-3 border border-black w-96 h-32 bg-white"
+      className="grid grid-cols-3 grid-rows-4 gap-2 border border-black max-w-md h-36 bg-white rounded-lg p-2"
       onClick={() => {
         changeProject({ ...project })
       }}
     >
-      <div className="col-span-1 bg-gray-300">ここ画像</div>
-      <div className="col-span-2 grid-rows-4 justify-center">
-        <div className="font-bold row-span-1 h-10 overflow-y-scroll">
-          {project.title}
-        </div>
-        {/* 多分伸ばせるようにしたほうがいいかも or modalとか挟んでも良さそう */}
-        <div className="row-span-2 overflow-y-scroll h-16">
-          {project.explanation}
-        </div>
-        {/* <div className="font-bold row-span-1">説明</div> */}
-        <div className="font-bold row-span-1 h-2">
-          <EarnedValue EarnedValue={project.Progress} />
-        </div>
+      <div className="col-span-1 bg-gray-300 rounded-lg row-span-4">
+        ここ画像
+      </div>
+      <div className="font-bold row-span-1 col-span-2 truncate">
+        {project.title}
+      </div>
+      <div className="row-span-2 col-span-2 overflow-y-scroll">
+        {project.explanation}
+      </div>
+      <div className="font-bold row-span-1 col-span-2 self-center">
+        <EarnedValue EarnedValue={project.Progress} />
       </div>
     </div>
   )

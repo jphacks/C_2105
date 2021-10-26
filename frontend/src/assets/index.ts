43e7bab1e3cb9a942202240ebcb2b3cebc9c9c91
toshikingggg <study.king7777@gmail.com>
2021-10-27 00:58:46 +0900

    loading画面にgalleryを追加

---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,9 @@
+import family from './family.jpg'
+import colleagues from './colleagues.jpg'
+import global from './global.jpg'
+
+export const imgMap = {
+  family: family,
+  colleagues: colleagues,
+  global: global,
+}
--- frontend/src/pages/LoadingOnFundraising.tsx
+++ frontend/src/pages/LoadingOnFundraising.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { useHistory } from 'react-router-dom'
+import { Gallery } from '../components/Gallery'
 import { useProjectContext } from '../context/ProjectProvider'
 
 export const LoadingOnFundraising: FC = () => {
@@ -8,6 +9,11 @@ export const LoadingOnFundraising: FC = () => {
   //TODO バックエンド側の金額配分API叩く処理
   setTimeout(() => history.push(`/${selectedProject!.id}/result`, 1000), 5000)
   return (
-    <button className="btn btn-sm btn-ghost loading text-2xl">募金中...</button>
+    <>
+      <button className="btn btn-sm btn-ghost loading text-3xl absolute top-24">
+        募金中...
+      </button>
+      <Gallery />
+    </>
   )
 }

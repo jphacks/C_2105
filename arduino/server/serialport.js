c4a84805e175d200e2381fbf3af3a407b9c46f4c
Hayato Mitomi <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-10-26 19:31:42 +0900

    [add] serialport通信後の処理

--- arduino/server/serialport.js
+++ arduino/server/serialport.js
@@ -17,9 +17,25 @@ const sp = new Serialport(process.env.PORT_NAME, {
   baudRate: 115200,
 });
 
+const coinsSortedBySize = ['1', '50', '5', '100', '10', '500'];
+
+let counter = new Array(7).fill(0);
+
 sp.on('data', (data) => {
   try {
-    console.log(data.toString());
+    const value = data.toString();
+    if(value == 'end'){
+      // 一旦投入に区切りがついた
+      const donatedMoney = {};
+      for(let i = 0; i < 6; i++){
+        donatedMoney[coinsSortedBySize[i]] = counter[i] - counter[i + 1];
+      }
+      // イベント名は変えるかも
+      socket.emit('donated', donatedMoney);
+      counter = new Array(7).fill(0);
+    }else{
+      counter[value]++;
+    }
   } catch(e) {
     console.log(e);
     return;

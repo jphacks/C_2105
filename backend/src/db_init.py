1d9564e44dcbc24300592cbee529b63263a2ee9f
Kaisei Sato <s2121635@s.tsukuba.ac.jp>
2021-10-28 22:01:25 +0900

    Fix: db

--- backend/src/db_init.py
+++ backend/src/db_init.py
@@ -1,18 +1,18 @@
 from app import db, Project, Log
 
 def initialize():
-  p = Project(101, 'プロジェクト1', 'プロジェクト1の説明です。', 0, 'hoge.jpg', 100)
+  p = Project(101, 'プロジェクト1', 'プロジェクト1の説明です。', 50, 'hoge.jpg', 100)
   db.session.add(p)
-  p = Project(102, 'プロジェクト2', 'プロジェクト2の説明です。', 0, 'hoge.jpg', 100)
+  p = Project(102, 'プロジェクト2', 'プロジェクト2の説明です。', 40, 'hoge.jpg', 100)
   db.session.add(p)
   p = Project(103, 'プロジェクト3', 'プロジェクト3の説明です。', 0, 'hoge.jpg', 100)
   db.session.add(p)
 
-  l = Log(501, 101, '202110250930', 20, 'selected')
+  l = Log(50001, 101, '20211025093055', 20, 'selected')
   db.session.add(l)
-  l = Log(502, 101, '202110250931', 30, 'selected')
+  l = Log(50002, 101, '20211025093155', 30, 'selected')
   db.session.add(l)
-  l = Log(503, 102, '202110250932', 40, 'auto')
+  l = Log(50003, 102, '20211025093255', 40, 'auto')
   db.session.add(l)
 
   db.session.commit()

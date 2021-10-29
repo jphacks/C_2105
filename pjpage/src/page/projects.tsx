e7af2a3cdb6af059e934f0b4eeeac2fe8fdb272f
minami1102 <m.ompp38@gmail.com>
2021-10-29 18:59:19 +0900

    見た目

--- pjpage/src/page/projects.tsx
+++ pjpage/src/page/projects.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from 'react';
 import queryString from 'query-string';
 import { dataProps, projectData } from '../dummyData/dummyProjects';
-import './projects.module.css';
+import 'bulma/css/bulma.css';
+import './projects.css';
 
 type Props = {}
 
@@ -26,41 +27,61 @@ export const Projects: FC<Props> = () => {
   ];
 
   return (
-    <>
-      <div className={`${!project && 'is-hidden'}`}>
-        
-        <span className="is-size-5">スマート貯金箱</span>
-        <br />
-        <img className="Main-image" src={project?.img} alt={project?.title} />
-
-        <h1>
-          {project?.title}
-        </h1>
-
-        <h2>概要</h2>
-        <p>
-          {project?.explanation}
-        </p>
-
-        <h2>寄付ログ</h2>
-        { latestDonateHistory.map((data) => (
-          <p>{`${data?.date} ${data?.money}円の寄付がありました。`}</p>
-        )) }
-
-        <h2>プロジェクトからのお知らせ</h2>
-        { latestNews.map((data) => (
-          <div className="columns is-mobile card">
-            <img className="column is-3" src={data?.img} alt={data?.title} />
-             <div className="column is-narrow">
-               <h3>{data?.title}</h3>
-               <p>{data?.body}</p>
-               <p>{data?.date}</p>
-             </div>
+    <div className="pjpage has-text-white pb-5">
+      <div className={`${!project && 'is-hidden'} container`}>
+        <div className="sub-container">
+          <div>
+            <p className="is-size-5 has-text-weight-bold p-4">スマート貯金箱</p>
           </div>
-        )) }
+          <div className="">
+            <img className="top-image" src={project?.img} alt={project?.title} />
+          </div>
 
-      </div>
-    </>
+          <h1 className="is-size-4 has-text-weight-bold p-4">
+            {project?.title}
+          </h1>
+
+          <div className="has-text-left my-5">
+            <h2 className="is-size-4 has-text-weight-semibold">
+              概要
+            </h2>
+            <p>
+              {project?.explanation}
+            </p>
+          </div>
+
+          <div className="has-text-left my-5">
+            <h2 className="is-size-4 has-text-weight-semibold">
+              寄付ログ
+            </h2>
+            <ul>
+              { latestDonateHistory.map((data) => (
+                <li>{`${data?.date} ${data?.money}円の寄付がありました。`}</li>
+              )) }
+            </ul>
+          </div>
+
+          <div className="has-text-left my-5">
+            <h2 className="is-size-4 has-text-weight-semibold">
+              プロジェクトからのお知らせ
+            </h2>
+            { latestNews.map((data) => (
+              <a href="/?no=2" className="columns is-mobile card m-2">
+                <img className="column is-3" src={data?.img} alt={data?.title} />
+                <div className="column is-9 is-relative">
+                  <h3 className="is-size-5 has-text-weight-semibold">{data?.title}</h3>
+                  <p className="mb-4">{data?.body}</p>
+                  <p className="news-update">{data?.date}</p>
+                </div>
+              </a>
+            )) }
+          </div>
+
+        </div>
+
+        </div>
+        
+    </div>
   )
 }
 

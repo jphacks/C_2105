3df3d73f652ee0077a5ac9c9fdd6b9e83418dee4
toshikingggg <study.king7777@gmail.com>
2021-10-29 16:29:34 +0900

    [fix]img path

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -2,12 +2,12 @@ import family from './family.jpg'
 import colleagues from './colleagues.jpg'
 import global from './global.jpg'
 import noImage from './noimage.jpg'
-import inu from './backendImages/inu.jpg'
-import onemother from './backendImages/onemother.jpg'
-import iryo from './backendImages/iryo.jpg'
-import yasei from './backendImages/yasei.jpg'
-import vote from './backendImages/vote.jpg'
-import atami from './backendImages/atami.jpg'
+import inu from './images/inu.jpg'
+import onemother from './images/onemother.jpg'
+import iryo from './images/iryo.jpg'
+import yasei from './images/yasei.jpg'
+import vote from './images/vote.jpg'
+import atami from './images/atami.jpg'
 
 export const imgMap = {
   family: family,
